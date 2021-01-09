62754bd8450d8bd8351cfe247d16e14567df0008
me4hit <nghiahuawei10101999@gmail.com>
2021-01-10 02:34:49 +0700

    Configure to deloy up herou

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const http = require('http').Server(app);
 global.io = require('socket.io')(http);
 
